49d9fd3e37464c62557d0ac5ff6121ff6f842b23
soyeong.kwon <soyeong_k@naver.com>
2020-03-06 17:31:31 +0900

    jupyter seminar
    Signed-off-by: soyeong.kwon <soyeong_k@naver.com>

--- test/cookiecutter_test/test_rete/js/lib/rete_test.js
+++ test/cookiecutter_test/test_rete/js/lib/rete_test.js
@@ -11,20 +11,87 @@ var _ = require('lodash');
 
 class NumComponent extends Rete.Component {
     constructor() {
-        super('Number');
+        super("Number");
     }
 
     builder(node) {
-        let out = new Rete.Output('num', 'Number', numSocket);
+        var out1 = new Rete.Output("num", "Value", numSocket);
 
-        node.addOutput(out);
+        return node
+            // .addControl(new NumControl(this.editor, "num"))
+            .addOutput(out1);
     }
 
     worker(node, inputs, outputs) {
-        outputs['num'] = node.data.num;
+        outputs["num"] = node.data.num;
     }
 }
 
+class MathComponent extends Rete.Component {
+    doOperation(v1, v2) {
+        return 0;
+    }
+
+    builder(node) {
+        var inp1 = new Rete.Input("num1", "Value 1", numSocket);
+        var inp2 = new Rete.Input("num2", "Value 2", numSocket);
+        var out = new Rete.Output("num", "Result", numSocket);
+
+        // inp1.addControl(new NumControl(this.editor, "num1"));
+        // inp2.addControl(new NumControl(this.editor, "num2"));
+
+        return node
+            .addInput(inp1)
+            .addInput(inp2)
+            // .addControl(new NumControl(this.editor, "preview", true))
+            .addOutput(out);
+    }
+
+    worker(node, inputs, outputs) {
+        var n1 = inputs["num1"].length ? inputs["num1"][0] : node.data.num1;
+        var n2 = inputs["num2"].length ? inputs["num2"][0] : node.data.num2;
+        var sum = this.doOperation(n1, n2);
+
+        this.editor.nodes
+            .find(n => n.id == node.id)
+            .controls.get("preview")
+            .setValue(sum);
+        outputs["num"] = sum;
+    }
+}
+
+class AddComponent extends MathComponent {
+    constructor() {
+        super("Add");
+    }
+    doOperation(v1, v2) {
+        return v1 + v2;
+    }
+}
+class SubtractComponent extends MathComponent {
+    constructor() {
+        super("Subtract");
+    }
+    doOperation(v1, v2) {
+        return v1 - v2;
+    }
+}
+class MultiplyComponent extends MathComponent {
+    constructor() {
+        super("Multiply");
+    }
+    doOperation(v1, v2) {
+        return v1 * v2;
+    }
+}
+class DivideComponent extends MathComponent {
+    constructor() {
+        super("Divide");
+    }
+    doOperation(v1, v2) {
+        return v2 != 0 ? v1 / v2 : 0;
+    }
+}
 
 // See example.py for the kernel counterpart to this file.
 
@@ -101,6 +168,14 @@ var ReteView = widgets.DOMWidgetView.extend({
 
         // const container = document.querySelector('#rete');
         const editor = new Rete.NodeEditor('demo@0.1.0', container);
+        const engine = new Rete.Engine('demo@0.1.0');
+        let components = [
+            new NumComponent(),
+            new AddComponent(),
+            new SubtractComponent(),
+            new MultiplyComponent(),
+            new DivideComponent()
+        ];
 
         editor.use(ConnectionPlugin);
         editor.use(VueRenderPlugin);
@@ -120,31 +195,34 @@ var ReteView = widgets.DOMWidgetView.extend({
             }
         });
 
-        const numComponent = new NumComponent();
-        editor.register(numComponent);
-
-        const engine = new Rete.Engine('demo@0.1.0');
-        engine.register(numComponent);
-
-        editor.on('process nodecreated noderemoved connectioncreated connectionremoved', async() => {
-            await engine.abort();
-            await engine.process(editor.toJSON());
+        components.map(c => {
+            editor.register(c);
+            engine.register(c);
         });
 
-        var n1 = await numComponent.createNode({ num: 2 });
-        var n2 = await numComponent.createNode({ num: 0 });
+        let n1 = await components[0].createNode({ num: 2 });
+        let n2 = await components[0].createNode({ num: 3 });
+        let add = await components[1].createNode();
 
         n1.position = [80, 200];
         n2.position = [80, 400];
+        add.position = [500, 240];
 
         editor.addNode(n1);
         editor.addNode(n2);
+        editor.addNode(add);
 
-        editor.on('process nodecreated noderemoved connectioncreated connectionremoved', async() => {
-            console.log('process');
-            await engine.abort();
-            await engine.process(editor.toJSON());
-        });
+        editor.connect(n1.outputs.get("num"), add.inputs.get("num1"));
+        editor.connect(n2.outputs.get("num"), add.inputs.get("num2"));
+
+        editor.on(
+            "process nodecreated noderemoved connectioncreated connectionremoved",
+            async() => {
+                console.log("processing...");
+                await engine.abort();
+                await engine.process(editor.toJSON());
+            }
+        );
 
         //editor.view.resize();
         AreaPlugin.zoomAt(editor);
